90765fb2c6757edc2d20e3a03c73272ffefe9090
Janson Lau <jansonla@usc.edu>
2016-09-19 20:01:14 -0700

    first commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Janson Lau on 9/13/16.
+//  Copyright © 2016 Janson Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var billField: UITextField!
+
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    let tipPercentages = [0.10, 0.15, 0.20]
+
+    @IBAction func calculateTip(sender: AnyObject) {
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+}
+
